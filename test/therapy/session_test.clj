93cc1542bfdf5375a7c0d94fab7e278cff777d55
ken restivo <krestivo@restivo.org>
2012-06-22 22:15:03 -0700

    Use testing macro as suggested by Raynes

--- test/therapy/session_test.clj
+++ test/therapy/session_test.clj
@@ -4,28 +4,28 @@
 
 (deftest therapy-session
   (let [base-map {:uri "/foo" :request-method :get }]
-    ;; put session value in
+    (testing "Put session value in")
     (is (= "bar" (get-in ((wrap-therapy-session-existing
                            #(assoc-in % [:session :foo] "bar"))
                           base-map)
                          [:session :foo])))
     (let [base-map (assoc base-map :session {:foo "bar"})]
-      ;; pass session value through
+      (testing "Pass session value through")
       (is (= "bar" (get-in ((wrap-therapy-session-existing identity)
                             base-map)
                            [:session :foo])))
-      ;; change session value
+      (testing "Change session value")
       (is  (= "baz" (get-in ((wrap-therapy-session-existing
                               #(assoc-in % [:session :foo] "baz"))
                              base-map)
                             [:session :foo])))
-      ;; dissoc session value
+      (testing "Dissoc session value")
       (is  (not (contains? (:session
                             ((wrap-therapy-session-existing
                               #(assoc % :session (dissoc (:session %) :foo)))
                              base-map))
                            :foo))))
-    ;; dissocing one key doesn't affect any others
+    (testing "Dissocing one key doesn't affect any others")
     (let [base-map (assoc base-map :session {:foo "bar" :quuz "auugh"})
           part-dissoc (:session
                        ((wrap-therapy-session-existing
@@ -33,19 +33,19 @@
                         base-map))]
       (is (not (contains? part-dissoc :foo)))
       (is (= "auugh" (:quuz  part-dissoc)))
-      ;; changing one key doesn't affect any others
+      (testing "Changing one key doesn't affect any others")
       (let [part-change (:session
                          ((wrap-therapy-session-existing
                            #(assoc-in % [:session :foo] "baz"))
                           base-map))]
         (is (= "baz" (:foo part-change)))
         (is (= "auugh" (:quuz  part-change)))))
-    ;; delete whole session.
-    ;; ring takes nil to mean delete session, so it must get passed through
+    (testing "Delete whole session.
+     Ring takes nil to mean delete session, so it must get passed through.")
     (is (nil?  (:session ((wrap-therapy-session-existing
                            #(assoc % :session nil))
                           base-map))))
-    ;; make sure the whole session goes away and stays away if deleted
+    (testing "Make sure the whole session goes away and stays away if deleted")
     (is (not (contains?  ((wrap-therapy-session-existing
                            #(dissoc % :session))
                           base-map)
